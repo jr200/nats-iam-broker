154eb25720f0c7103bf9fd5d928aeebc4bc7d3fa
Tommy Lindgren <tommy.lindgren@plejd.com>
2025-04-18 17:04:58 +0200

    refactor: Changes after fixing linting/shellcheck

--- scripts/vault-toolkit.sh
+++ scripts/vault-toolkit.sh
@@ -8,15 +8,18 @@ VAULT_CMD=vault
 function render_jq_template() {
   local template_file=$1
   shift
-  local kv_pairs=("$@")
+  local kv_pairs=("${@}")
   
   # Read the template file into a variable
-  local template=$(<"$template_file")
+  local template
+  template=$(<"$template_file")
 
   # Iterate over the key-value pairs and perform the substitutions
   for kv in "${kv_pairs[@]}"; do
-    local key=$(echo "$kv" | cut -d= -f1)
-    local value=$(echo "$kv" | cut -d= -f2-)
+    local key
+    key=$(echo "$kv" | cut -d= -f1)
+    local value
+    value=$(echo "$kv" | cut -d= -f2-)
     
     # Perform the substitution in the template using jq
     # Determine if the value is a JSON array or object
@@ -37,13 +40,14 @@ function create_user_minter() {
 
   template_file="${SCRIPT_DIR}/templates/user.json.j2"
 
-  json_payload=`render_jq_template ${template_file} \
+  # shellcheck disable=SC2016
+  json_payload=$(render_jq_template "${template_file}" \
     ".useSigningKey=${account}-ask1" \
     '.claims.name=minter' \
     ".claims.user.pub.allow=[\">\"]" \
     '.claims.user.sub.allow=["_INBOX.>", "$SRV.>", "$SYS.REQ.USER.*"]' \
-    `
-	echo ${json_payload} | $VAULT_CMD write nats/issue/operator/${operator}/account/${account}/user/minter -
+    )
+	echo "${json_payload}" | $VAULT_CMD write "nats/issue/operator/${operator}/account/${account}/user/minter" -
 }
 
 function create_user_nobody() {
@@ -52,13 +56,13 @@ function create_user_nobody() {
 
   template_file="${SCRIPT_DIR}/templates/user.json.j2"
 
-  json_payload=`render_jq_template ${template_file} \
+  json_payload=$(render_jq_template "${template_file}" \
     ".useSigningKey=${account}-ask1" \
     '.claims.name=nobody' \
     ".claims.user.pub.deny=[\">\"]" \
     ".claims.user.sub.deny=[\">\"]" \
-    `
-	echo ${json_payload} | $VAULT_CMD write nats/issue/operator/${operator}/account/${account}/user/nobody -
+    )
+	echo "${json_payload}" | $VAULT_CMD write "nats/issue/operator/${operator}/account/${account}/user/nobody" -
 }
 
 function create_user_god() {
@@ -67,20 +71,20 @@ function create_user_god() {
 
   template_file="${SCRIPT_DIR}/templates/user.json.j2"
 
-  json_payload=`render_jq_template ${template_file} \
+  json_payload=$(render_jq_template "${template_file}" \
     ".useSigningKey=${account}-ask1" \
     '.claims.name=god' \
     ".claims.user.pub.allow=[\">\"]" \
     ".claims.user.sub.allow=[\">\"]" \
-    `
-	echo ${json_payload} | $VAULT_CMD write nats/issue/operator/${operator}/account/${account}/user/god -
+    )
+	echo "${json_payload}" | $VAULT_CMD write "nats/issue/operator/${operator}/account/${account}/user/god" -
 }
 
 function read_account_pubkey() {
   local operator=$1
   local account=$2
 
-  account_pubkey=`$VAULT_CMD read -format=table -field=publicKey "nats/nkey/operator/${operator}/account/${account}"`
+  account_pubkey=$($VAULT_CMD read -format=table -field=publicKey "nats/nkey/operator/${operator}/account/${account}")
   echo "${account_pubkey}"
 }
 
@@ -88,7 +92,7 @@ function read_account_signing_key() {
   local operator=$1
   local account=$2
 
-  account_signing_key=`$VAULT_CMD read -format=table -field=seed "nats/nkey/operator/${operator}/account/${account}"`
+  account_signing_key=$($VAULT_CMD read -format=table -field=seed "nats/nkey/operator/${operator}/account/${account}")
   echo "${account_signing_key}"
 }
 
@@ -97,7 +101,7 @@ function read_user_pubkey() {
   local account=$2
   local user=$3
 
-  user_pubkey=`$VAULT_CMD read -format=table -field=publicKey "nats/nkey/operator/${operator}/account/${account}/user/${user}"`
+  user_pubkey=$($VAULT_CMD read -format=table -field=publicKey "nats/nkey/operator/${operator}/account/${account}/user/${user}")
   echo "${user_pubkey}"
 }
 
@@ -106,7 +110,8 @@ function read_user_creds() {
   local account=$2
   local user=$3
 
-  local user_creds=`$VAULT_CMD read -format=table -field=creds "nats/creds/operator/${operator}/account/${account}/user/${user}"`
+  local user_creds
+  user_creds=$($VAULT_CMD read -format=table -field=creds "nats/creds/operator/${operator}/account/${account}/user/${user}")
   echo "${user_creds}"
 }
 
@@ -119,8 +124,8 @@ function save_nats_context() {
   local path=${NATS_CREDS_STORE:-~/.nats}
   mkdir -p "${path}"
 
-  creds=`read_user_creds "${operator}" "${account}" "${user}"`
-  echo "${creds}" > ${path}/u-${account}-${user}.creds
+  creds=$(read_user_creds "${operator}" "${account}" "${user}")
+  echo "${creds}" > "${path}/u-${account}-${user}.creds"
 
   save_name=$(echo "vault-nats_${account}_${user}" | awk '{print tolower($0)}')
 
@@ -129,7 +134,7 @@ function save_nats_context() {
   nats context save "${save_name}" \
       --creds="${path}/u-${account}-${user}.creds" \
       --description "vault-nats: ${operator}/${account}/${user}" \
-      "$@"
+      "${@}"
 }
 
 
@@ -138,12 +143,13 @@ function create_plain_account() {
   local account=$2
   template_file="${SCRIPT_DIR}/templates/account.json.j2"
 
-  account_json=`render_jq_template ${template_file} \
+  account_json=$(render_jq_template "${template_file}" \
     '.useSigningKey=opsk1' \
     ".claims.name=${account}" \
-    ".claims.account.signingKeys=[\"${account}-ask1\"]"`
+    ".claims.account.signingKeys=[\"${account}-ask1\"]" \
+    )
 
-	echo ${account_json} | $VAULT_CMD write nats/issue/operator/${operator}/account/${account} -
+	echo "${account_json}" | $VAULT_CMD write "nats/issue/operator/${operator}/account/${account}" -
 
   # account_signing_key=`read_account_signing_key ${operator} ${account}`
 }
@@ -153,22 +159,23 @@ function create_minting_account() {
   local account=$2
   template_file="${SCRIPT_DIR}/templates/account.json.j2"
 
-  create_user_minter ${operator} ${account}
-  minter_pubkey=`read_user_pubkey ${operator} ${account} minter`
-  create_user_nobody ${operator} ${account}
+  create_user_minter "${operator}" "${account}"
+  minter_pubkey=$(read_user_pubkey "${operator}" "${account}" minter)
+  create_user_nobody "${operator}" "${account}"
 
 	nsc generate nkey --curve 2> ./tmp/acct-encryption-key.xk
-  encryption_pubkey=`sed -n 2,1p ./tmp/acct-encryption-key.xk`
+  encryption_pubkey=$(sed -n 2,1p ./tmp/acct-encryption-key.xk)
 
-  account_json=`render_jq_template ${template_file} \
+  account_json=$(render_jq_template "${template_file}" \
     '.useSigningKey=opsk1' \
     ".claims.name=${account}" \
     ".claims.account.signingKeys=[\"${account}-ask1\"]" \
     ".claims.account.authorization.auth_users=[\"${minter_pubkey}\"]" \
     '.claims.account.authorization.allowed_accounts=["*"]' \
-    ".claims.account.authorization.xkey=${encryption_pubkey}"`
+    ".claims.account.authorization.xkey=${encryption_pubkey}" \
+    )
 
-	echo ${account_json} | $VAULT_CMD write nats/issue/operator/${operator}/account/${account} -
+	echo "${account_json}" | $VAULT_CMD write "nats/issue/operator/${operator}/account/${account}" -
 
   # account_signing_key=`read_account_signing_key ${operator} ${account}`
 }
